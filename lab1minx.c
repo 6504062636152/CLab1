05a236f516df4ff8be3199988c71b52ced4264e0
6504062636152 <153051520+6504062636152@users.noreply.github.com>
2024-11-28 18:08:57 +0700

    v2 if-else

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	int num1=10;
+	int num1=20;
 	int num2=30;
 	if (num1>num2){
 		printf("num1 is greater than num2");
-	}
-	if (num2>num1){
+	}else {
 		printf("num2 is greater than num1");
 	}
 }
